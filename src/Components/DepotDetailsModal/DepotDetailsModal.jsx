e260a088b8f90b84984e31800955ecba23729908
TouhedurRahman <86222384+TouhedurRahman@users.noreply.github.com>
2024-11-24 10:18:05 +0600

    depot stock in list added

--- src/Components/DepotDetailsModal/DepotDetailsModal.jsx
+++ src/Components/DepotDetailsModal/DepotDetailsModal.jsx
@@ -32,7 +32,7 @@ const DepotDetailsModal = ({ isOpen, onClose, product }) => {
                             {/* Product Details */}
                             <div className="bg-white rounded-lg shadow-md">
                                 <table className="w-full text-left border-collapse">
-                                    <tbody>
+                                    <tbody className='text-center'>
                                         <tr className="border-b">
                                             <th className="py-2 text-green-700 font-semibold">Code</th>
                                             <td className="py-2 text-gray-700">{product.productCode}</td>
@@ -65,15 +65,15 @@ const DepotDetailsModal = ({ isOpen, onClose, product }) => {
                                 <thead>
                                     <tr className="bg-blue-100">
                                         <th className="px-4 py-2 border border-gray-200">Price Type</th>
-                                        <th className="px-4 py-2 border border-gray-200">Unit Price (৳)</th>
-                                        <th className="px-4 py-2 border border-gray-200">Total Price (৳)</th>
+                                        <th className="px-4 py-2 border border-gray-200 text-right">Unit Price (৳)</th>
+                                        <th className="px-4 py-2 border border-gray-200 text-right">Total Price (৳)</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     <tr>
                                         <td className="px-4 py-2 border border-gray-200">Trade Price</td>
-                                        <td className="px-4 py-2 border border-gray-200">{product.tradePrice.toLocaleString('en-IN')}/-</td>
-                                        <td className="px-4 py-2 border border-gray-200">{totalTradePrice.toLocaleString('en-IN')}/-</td>
+                                        <td className="px-4 py-2 border border-gray-200 text-right">{product.tradePrice.toLocaleString('en-IN')}/-</td>
+                                        <td className="px-4 py-2 border border-gray-200 text-right">{totalTradePrice.toLocaleString('en-IN')}/-</td>
                                     </tr>
                                 </tbody>
                             </table>
