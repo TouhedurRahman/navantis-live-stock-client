379f30db6f984e83eccf1a87da7232f33588577c
TouhedurRahman <86222384+TouhedurRahman@users.noreply.github.com>
2024-12-26 11:53:17 +0600

    authprovider added

---
+++ src/Providers/AuthProvider/AuthProvider.jsx
@@ -0,0 +1,103 @@
+import {
+    createUserWithEmailAndPassword,
+    getAuth,
+    GoogleAuthProvider,
+    onAuthStateChanged,
+    sendEmailVerification,
+    sendPasswordResetEmail,
+    signInWithEmailAndPassword,
+    signInWithPopup,
+    signOut,
+    updateProfile
+} from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import app from "../../Firebase/firebase.config";
+
+export const AuthContext = createContext(null);
+
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    // Google provider
+    const googleProvider = new GoogleAuthProvider();
+
+    const createUser = async (email, password) => {
+        setLoading(true);
+        try {
+            const result = await createUserWithEmailAndPassword(auth, email, password);
+            // Send verification email
+            sendEmailVerification(result.user)
+                .then(() => {
+                    console.log('Verification email sent to:', email);
+                })
+                .catch(error => {
+                    console.error('Error sending verification email:', error);
+                });
+            return result;
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    const logIn = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const googleLogIn = () => {
+        setLoading(true);
+        return signInWithPopup(auth, googleProvider);
+    }
+
+    const resetPassword = (email) => {
+        return sendPasswordResetEmail(auth, email);
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            if (currentUser?.emailVerified) {
+                setUser(currentUser);
+                // console.log('Current user: ', currentUser.email);
+            } else if (currentUser) {
+                setUser(currentUser);
+            }
+            setLoading(false);
+        });
+        return () => {
+            unsubscribe();
+        }
+    }, []);
+
+    const updateUserProfile = (userInfo) => {
+        updateProfile(auth.currentUser, userInfo);
+    };
+
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+
+    const authInfo = {
+        user,
+        loading,
+        createUser,
+        logIn,
+        googleLogIn,
+        updateUserProfile,
+        resetPassword,
+        logOut
+    }
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {
+                children
+            }
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -6,16 +6,19 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
-import "./index.css";
-import router from './Routes/Routes/Routes';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import "./index.css";
+import AuthProvider from './Providers/AuthProvider/AuthProvider';
+import router from './Routes/Routes/Routes';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
