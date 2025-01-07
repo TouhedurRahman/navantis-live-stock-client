c3fe127f699ce71394eb82f371f6d7e38bea6c7c
TouhedurRahman <86222384+TouhedurRahman@users.noreply.github.com>
2025-01-07 14:55:19 +0600

    get single user hook added

---
+++ src/Hooks/useSingleUser.jsx
@@ -0,0 +1,19 @@
+import { useQuery } from '@tanstack/react-query';
+import useAuth from './useAuth';
+
+const useSingleUser = () => {
+    const { user } = useAuth();
+
+    const { data: singleUser = {}, isLoading: loadingSingleUser, refetch } = useQuery({
+        queryKey: ['singleUser', user?.email],
+        queryFn: async () => {
+            const url = `http://localhost:5000/user/${user.email}`;
+            const result = await fetch(url);
+            return result.json();
+        }
+    })
+
+    return [singleUser, loadingSingleUser, refetch];
+};
+
+export default useSingleUser;
