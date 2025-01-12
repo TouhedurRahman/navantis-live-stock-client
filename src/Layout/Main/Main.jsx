7a6276a60c819314c5b5fe7847299097007a8f3f
TouhedurRahman <86222384+TouhedurRahman@users.noreply.github.com>
2025-01-12 16:13:22 +0600

    login with veification and eemail, password added

--- src/Layout/Main/Main.jsx
+++ src/Layout/Main/Main.jsx
@@ -1,4 +1,52 @@
 import React from 'react';
+import { ToastContainer } from 'react-toastify';
+import Loader from '../../Components/Loader/Loader';
+import NonVerified from '../../Components/NonVerified/NonVerified';
+import NotAssignedDesignation from '../../Components/NotAssignedDesignation/NotAssignedDesignation';
+import useAuth from '../../Hooks/useAuth';
+import useSingleUser from '../../Hooks/useSingleUser';
+import Navbar from '../../Pages/Shared/Navbar/Navbar';
+import Login from '../../Routes/Routes/LoginAndRegistration/Login/Login';
+
+const Main = () => {
+    const { user } = useAuth();
+    const [singleUser, loadingSingleUser] = useSingleUser();
+
+    if (!user) {
+        return <Login />;
+    }
+
+    if (!user.emailVerified) {
+        return <NonVerified />;
+    }
+
+    return (
+        <div className="font-nunito">
+            {
+                loadingSingleUser
+                    ?
+                    <>
+                        <Loader />
+                    </>
+                    :
+                    <>
+                        {
+                            singleUser?.designation !== ''
+                                ?
+                                <Navbar />
+                                :
+                                <NotAssignedDesignation />
+                        }
+                    </>
+            }
+            <ToastContainer />
+        </div>
+    );
+};
+
+export default Main;
+
+/* import React from 'react';
 import Navbar from '../../Pages/Shared/Navbar/Navbar';
 
 const Main = () => {
@@ -9,4 +57,4 @@ const Main = () => {
     );
 };
 
-export default Main;
+export default Main; */
