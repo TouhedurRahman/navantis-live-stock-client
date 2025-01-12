2e2b248d61e85c0246dbfdbb9fdc8ba6c9e2449d
TouhedurRahman <86222384+TouhedurRahman@users.noreply.github.com>
2025-01-12 14:50:04 +0600

    allowed designation added in menu config file

--- src/Hooks/useMenuConfig.jsx
+++ src/Hooks/useMenuConfig.jsx
@@ -6,6 +6,76 @@ import { GiDustCloud } from 'react-icons/gi';
 import { IoIosAddCircle } from 'react-icons/io';
 import { TbCoinTakaFilled } from 'react-icons/tb';
 import { VscGitPullRequestGoToChanges } from 'react-icons/vsc';
+import useSingleUser from './useSingleUser';
+
+const useMenuConfig = () => {
+    const [singleUser] = useSingleUser();
+
+    const allowedDesignations = {
+        admin: ["Managing Director"],
+        warehouse: ["Managing Director", "Warehouse Incharge"],
+        depot: ["Managing Director", "Depot Incharge"],
+    };
+
+    const baseMenuConfig = {};
+
+    if (allowedDesignations.admin.includes(singleUser?.designation)) {
+        baseMenuConfig.admin = {
+            icon: <FaUserCog className="mr-2" onClick={() => setSidebarOpen(true)} />,
+            links: [
+                { to: '/admin-po', icon: <FaShoppingCart className='me-2' />, label: 'Purchase Order' },
+                { to: '/purchase-list', icon: <FaListUl className='me-2' />, label: 'Purchase List' },
+                { to: '/warehouse-request', icon: <VscGitPullRequestGoToChanges className='me-2' />, label: 'Warehouse Request' },
+                { to: '/damage-request', icon: <VscGitPullRequestGoToChanges className='me-2' />, label: 'Damage Request' },
+                { to: '/depot-request', icon: <VscGitPullRequestGoToChanges className='me-2' />, label: 'Depot Request' },
+                { to: '/expire-request', icon: <VscGitPullRequestGoToChanges className='me-2' />, label: 'Expire Request' },
+                { to: '/missing-products', icon: <FaExclamationCircle className='me-2' />, label: 'Missing Products' },
+                { to: '/price-update', icon: <TbCoinTakaFilled className='me-2' />, label: 'Price Update' },
+                { to: '/dmg-exp', icon: <FaTrashAlt className='me-2' />, label: 'Damaged & Exp.' },
+            ],
+        };
+    }
+
+    if (allowedDesignations.warehouse.includes(singleUser?.designation)) {
+        baseMenuConfig.warehouse = {
+            icon: <FaWarehouse className="mr-2" onClick={() => setSidebarOpen(true)} />,
+            links: [
+                { to: '/add-product-warehouse', icon: <IoIosAddCircle className='me-2' />, label: 'Add new' },
+                { to: '/depot-delivery', icon: <FaTruck className='me-2' />, label: 'Depot Delivery' },
+                { to: '/warehouse-list', icon: <FaListUl className='me-2' />, label: 'List' },
+                { to: '/warehouse-in', icon: <FaArrowDown className='me-2' />, label: 'Stock In' },
+                { to: '/warehouse-out', icon: <FaArrowUp className='me-2' />, label: 'Stock Out' },
+                { to: '/damaged-in-warehouse', icon: <GiDustCloud className='me-2' />, label: 'Damaged' },
+            ],
+        };
+    }
+
+    if (allowedDesignations.depot.includes(singleUser?.designation)) {
+        baseMenuConfig.depot = {
+            icon: <FaTruck className="mr-2" onClick={() => setSidebarOpen(true)} />,
+            links: [
+                { to: '/depot-receive-request', icon: <VscGitPullRequestGoToChanges className='me-2' />, label: 'Receive Request' },
+                { to: '/depot-list', icon: <FaListUl className='me-2' />, label: 'List' },
+                { to: '/depot-in', icon: <FaArrowDown className='me-2' />, label: 'Stock In' },
+                { to: '/depot-out', icon: <FaArrowUp className='me-2' />, label: 'Stock Out' },
+                { to: '/depot-expired', icon: <FcExpired className='me-2' />, label: 'Expired' },
+            ],
+        };
+    }
+
+    return { menuConfig: baseMenuConfig };
+};
+
+export default useMenuConfig;
+
+/* import {
+    FaArrowDown, FaArrowUp, FaExclamationCircle, FaListUl, FaShoppingCart, FaTrashAlt, FaTruck, FaUserCog, FaWarehouse
+} from 'react-icons/fa';
+import { FcExpired } from 'react-icons/fc';
+import { GiDustCloud } from 'react-icons/gi';
+import { IoIosAddCircle } from 'react-icons/io';
+import { TbCoinTakaFilled } from 'react-icons/tb';
+import { VscGitPullRequestGoToChanges } from 'react-icons/vsc';
 
 const useMenuConfig = () => {
     const menuConfig = {
@@ -49,4 +119,4 @@ const useMenuConfig = () => {
     return { menuConfig };
 };
 
-export default useMenuConfig;
+export default useMenuConfig; */
