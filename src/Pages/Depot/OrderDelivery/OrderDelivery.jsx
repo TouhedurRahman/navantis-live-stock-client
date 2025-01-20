5bda7b27fc396ad1291241e1e6a8fabe5440eef2
TouhedurRahman <86222384+TouhedurRahman@users.noreply.github.com>
2025-01-20 16:51:39 +0600

    order delivery section page header added

--- src/Pages/Depot/OrderDelivery/OrderDelivery.jsx
+++ src/Pages/Depot/OrderDelivery/OrderDelivery.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import PageTitle from '../../../Components/PageTitle/PageTitle';
 
 const OrderDelivery = () => {
     return (
-        <div>
-
-        </div>
+        <>
+            <div>
+                <PageTitle
+                    from={"Depot"}
+                    to={"Order delivery"}
+                />
+            </div>
+            <div className="bg-white pb-1">
+                <div>
+                    <h1 className="px-6 py-3 font-bold">Order delivery</h1>
+                    <hr className='text-center border border-gray-500 mb-5' />
+                </div>
+            </div>
+        </>
     );
 };
 
