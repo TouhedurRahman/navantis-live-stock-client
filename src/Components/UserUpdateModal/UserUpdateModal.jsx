8b59080d858c46eaa972664f99ab5d6edb6a644b
TouhedurRahman <86222384+TouhedurRahman@users.noreply.github.com>
2025-02-22 10:54:45 +0600

    update some design and add extra input

--- src/Components/UserUpdateModal/UserUpdateModal.jsx
+++ src/Components/UserUpdateModal/UserUpdateModal.jsx
@@ -15,7 +15,7 @@ const hierarchy = {
 
 const UserUpdateModal = ({ user, onClose }) => {
     const [allUsers, loading, refetch] = useAllUsers();
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit, formState: { errors }, reset } = useForm();
     const [managers, setManagers] = useState([]);
 
     useEffect(() => {
@@ -39,7 +39,7 @@ const UserUpdateModal = ({ user, onClose }) => {
 
     return (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-60">
-            <div className="bg-white rounded-lg shadow-lg w-full max-w-lg h-4/5 flex flex-col" style={{ maxHeight: '90%' }}>
+            <div className="bg-white rounded-lg shadow-lg w-full max-w-lg max-h-4/5 flex flex-col" style={{ maxHeight: '90%' }}>
                 {/* Header */}
                 <div className="flex justify-between items-center px-5 py-4 border-b border-gray-200">
                     <h2 className="text-xl font-semibold">Update User</h2>
@@ -59,15 +59,27 @@ const UserUpdateModal = ({ user, onClose }) => {
                         <p className='text-sm'>{user.designation}</p>
                     </div>
 
+                    <div className="flex flex-col">
+                        <label className="text-[#6E719A] mb-1 text-sm">
+                            Territory <span className="text-red-500">*</span>
+                        </label>
+                        <input
+                            {...register("territory", { required: "Territory is required" })}
+                            placeholder="Enter territory name"
+                            className="border-gray-500 bg-white border p-2 text-sm"
+                        />
+                        {errors.territory && <p className="text-red-500 text-sm">{errors.territory.message}</p>}
+                    </div>
+
                     {parent && (
                         <div className="mt-5">
-                            <label htmlFor="parent" className="block text-sm font-medium text-gray-700">
-                                Select {parent}
+                            <label htmlFor="parent" className="text-[#6E719A] mb-1 text-sm">
+                                {parent}  <span className="text-red-500">*</span>
                             </label>
                             <select
                                 id="parent"
                                 {...register('parent')}
-                                className="mt-2 block w-full px-4 py-2 text-gray-700 bg-white border rounded-md"
+                                className="w-full border-gray-500 bg-white border p-2 text-sm"
                                 defaultValue=""
                             >
                                 <option value="">Select {parent}</option>
@@ -80,13 +92,13 @@ const UserUpdateModal = ({ user, onClose }) => {
 
                     {grandparent && (
                         <div className="mt-5">
-                            <label htmlFor="grandparent" className="block text-sm font-medium text-gray-700">
-                                Select {grandparent}
+                            <label htmlFor="grandparent" className="text-[#6E719A] mb-1 text-sm">
+                                {grandparent}
                             </label>
                             <select
                                 id="grandparent"
                                 {...register('grandparent')}
-                                className="mt-2 block w-full px-4 py-2 text-gray-700 bg-white border rounded-md"
+                                className="w-full border-gray-500 bg-white border p-2 text-sm"
                                 defaultValue=""
                             >
                                 <option value="">Select {grandparent}</option>
@@ -100,9 +112,9 @@ const UserUpdateModal = ({ user, onClose }) => {
                     <div>
                         <button
                             type="submit"
-                            className="w-full mx-auto my-5 ml-2 px-4 py-2 text-white bg-blue-500 rounded-md shadow-sm hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                            className="w-full mx-auto my-5 py-2 text-white bg-blue-500 rounded-md shadow-sm hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
                         >
-                            Save Changes
+                            Update
                         </button>
                     </div>
                 </form>
