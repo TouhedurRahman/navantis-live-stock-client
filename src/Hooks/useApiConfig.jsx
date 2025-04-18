c9892720d02bea65de9d64794ef23d7e21b7c4ce
TouhedurRahman <touhedur.cse@gmail.com>
2025-04-18 15:56:44 +0600

    unnecessary file removed

--- src/Hooks/useApiConfig.jsx
+++ src/Hooks/useApiConfig.jsx
@@ -1,7 +1,4 @@
 const useApiConfig = () => {
-    // const baseUrl = 'http://localhost:5000';
-    // VITE_BASE_URL = http://localhost:5000;
-
     const baseUrl = import.meta.env.VITE_BASE_URL;
     return baseUrl;
 };
