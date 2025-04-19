b27cfaddd3d259e1a7c9a84ed97814837388cb29
TouhedurRahman <86222384+TouhedurRahman@users.noreply.github.com>
2025-04-19 17:43:59 +0600

    depot stock report invoices updated

--- src/Invoices/ExpiredReturnsInvoice.jsx
+++ src/Invoices/ExpiredReturnsInvoice.jsx
@@ -1,4 +1,9 @@
 const ExpiredReturnsInvoice = ({ firstDate, lastDate, totalUniqueProducts, totalUnit, totalTP, filteredExReturns }) => {
+    const now = new Date().toLocaleString("en-US", {
+        year: "numeric", month: "long", day: "numeric",
+        hour: "2-digit", minute: "2-digit", second: "2-digit",
+        hour12: true
+    });
 
     const handlePrint = () => {
         const companyHeader = `
@@ -26,6 +31,9 @@ const ExpiredReturnsInvoice = ({ firstDate, lastDate, totalUniqueProducts, total
                 `Date <b>${firstDate}</b>`
             }</p>
             </div>
+            <div class="mb-1 text-sm text-gray-400 text-right italic">
+                    <h3 class="">Printed on ${now}</h3>
+            </div>
             <div style="margin-bottom: 20px; padding: 5px 15px; border: 1px solid #B2BEB5; border-radius: 3px;">
                 <p style="font-size: 11px; font-weight: bold; text-align: center; text-transform: uppercase;">
                     Summary
@@ -52,6 +60,7 @@ const ExpiredReturnsInvoice = ({ firstDate, lastDate, totalUniqueProducts, total
                     <tr>
                         <th style="text-align: center;">Sl.</th>
                         <th style="text-align: left;">Product Name</th>
+                        <th style="text-align: center;">Pack Size</th>
                         <th style="text-align: center;">Batch</th>
                         <th style="text-align: center;">Exp.</th>
                         <th style="text-align: right;">Quantity</th>
@@ -66,6 +75,7 @@ const ExpiredReturnsInvoice = ({ firstDate, lastDate, totalUniqueProducts, total
                             <tr>
                                 <td style="text-align: center;">${idx + 1}</td>
                                 <td>${product.productName}</td>
+                                <td style="text-align: center;">${product.netWeight}</td>
                                 <td style="text-align: center;">${product.batch}</td>
                                 <td style="text-align: center;">${product.expire}</td>
                                 <td style="text-align: right;">${product.totalQuantity}</td>
@@ -79,7 +89,7 @@ const ExpiredReturnsInvoice = ({ firstDate, lastDate, totalUniqueProducts, total
                 <tbody>
                     <tr>
                         <!-- Merged first four columns -->
-                        <td colspan="4" style="text-align: center; font-weight: bold;">Total</td>
+                        <td colspan="5" style="text-align: center; font-weight: bold;">Total</td>
                         <td style="text-align: right;">${totalUnit}</td>
                         <td style="text-align: right;"></td>
                         <td style="text-align: right;">${totalTP.toLocaleString('en-IN')}/-</td>
@@ -88,23 +98,6 @@ const ExpiredReturnsInvoice = ({ firstDate, lastDate, totalUniqueProducts, total
                 </tbody>
             </table>
         `;
-
-        const currentDateTime = new Date();
-        const formattedDateTime = currentDateTime.toLocaleString('en-US', {
-            year: 'numeric',
-            month: 'long',
-            day: 'numeric',
-            hour: '2-digit',
-            minute: '2-digit',
-            second: '2-digit',
-            hour12: true,
-        });
-
-        const formattedDateTimeWithAt = formattedDateTime.replace(', ', ', ');
-        const finalFormattedDateTime = formattedDateTimeWithAt.replace(
-            ", ",
-            ", "
-        );
 
         const newWindow = window.open();
         const styles = [...document.querySelectorAll('link[rel="stylesheet"], style')].map(
@@ -146,8 +139,8 @@ const ExpiredReturnsInvoice = ({ firstDate, lastDate, totalUniqueProducts, total
                                 white-space: nowrap;
                             }
                             /* Footer styles for all pages */
-                            body::after {
-                                content: "Printed on ${finalFormattedDateTime}";
+                            /* body::after {
+                                content: "Printed on ${now}";
                                 position: fixed;
                                 bottom: 0;
                                 left: 0;
@@ -156,7 +149,7 @@ const ExpiredReturnsInvoice = ({ firstDate, lastDate, totalUniqueProducts, total
                                 font-size: 12px;
                                 font-style: italic;
                                 color: #555;
-                            }
+                            } */
                         }
                     </style>
                 </head>
