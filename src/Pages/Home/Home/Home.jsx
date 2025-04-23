11458398524937ff974d2302a5ba3e93b6dbe27c
TouhedurRahman <86222384+TouhedurRahman@users.noreply.github.com>
2025-04-23 16:20:54 +0600

    home section updated

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import PageTitle from '../../../Components/PageTitle/PageTitle';
 
 const Home = () => {
     return (
         <div>
-            Navantis Live Stock
+            <div>
+                <PageTitle
+                    from={"Account"}
+                    to={"Dashboard"}
+                />
+            </div>
+            <div className="bg-white pb-1">
+                <div>
+                    <h1 className="px-6 py-3 font-bold">My dashboard</h1>
+                    <hr className='text-center border border-gray-500 mb-5' />
+                </div>
+                <div>
+                    <p className="text-gray-600 font-mono font-extrabold text-center mb-6">
+                        Comming soon...
+                    </p>
+                </div>
+            </div>
         </div>
     );
 };
