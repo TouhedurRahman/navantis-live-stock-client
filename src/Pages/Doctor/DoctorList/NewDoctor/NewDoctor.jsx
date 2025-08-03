608ae0795ef8428871da28063b2200652f9d098e
TouhedurRahman <86222384+TouhedurRahman@users.noreply.github.com>
2025-08-03 16:47:20 +0600

    new doctor functionality added

--- src/Pages/Doctor/DoctorList/NewDoctor/NewDoctor.jsx
+++ src/Pages/Doctor/DoctorList/NewDoctor/NewDoctor.jsx
@@ -1,8 +1,190 @@
+import { useState } from "react";
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
+import { ImSearch } from "react-icons/im";
+import useDoctors from "../../../../Hooks/useDoctors";
+import useSingleUser from "../../../../Hooks/useSingleUser";
+import DoctorCard from "../DoctorCard/DoctorCard";
 
 const NewDoctor = () => {
-    return (
-        <div>
+    const [singleUser] = useSingleUser();
+    const [doctors, loading, refetch] = useDoctors();
 
+    const [currentPage, setCurrentPage] = useState(1);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [doctorsPerPage, setDoctorsPerPage] = useState(5);
+
+    const myDoctors = doctors.filter(doctor =>
+        ["pending", "denied"].includes(doctor.status)
+        &&
+        (
+            doctor.territory === singleUser.territory
+            ||
+            doctor.parentTerritory === singleUser?.territory
+            ||
+            doctor.parentId == singleUser?._id
+            ||
+            doctor.grandParentId == singleUser?._id
+        )
+    );
+
+    const filteredDoctors = myDoctors.filter(doctor =>
+        doctor?.name?.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    const totalPages = Math.ceil(filteredDoctors.length / doctorsPerPage);
+
+    const startIndex = (currentPage - 1) * doctorsPerPage;
+    const endIndex = Math.min(startIndex + doctorsPerPage, filteredDoctors.length);
+
+    const currentDoctors = filteredDoctors.slice(startIndex, endIndex);
+
+    const changePage = (page) => {
+        setCurrentPage(page);
+    };
+
+    const handleSearch = (e) => {
+        setSearchTerm(e.target.value);
+        setCurrentPage(1);
+    };
+
+    const handleOrdersPerPageChange = (e) => {
+        setDoctorsPerPage(Number(e.target.value));
+        setCurrentPage(1);
+    };
+
+    return (
+        <div className="bg-white mt-3 pb-1">
+            {
+                myDoctors.length > 0
+                    ?
+                    <>
+                        {
+                            loading
+                                ?
+                                <>
+                                    <Loader />
+                                </>
+                                :
+                                <>
+                                    <div className="px-6">
+                                        <div className="mb-5 flex flex-col-reverse md:flex-row justify-center md:justify-between items-center">
+                                            <div className="mt-5 md:mt-0">
+                                                <label htmlFor="doctorsPerPage">Show</label>
+                                                <select
+                                                    id="doctorsPerPage"
+                                                    value={doctorsPerPage}
+                                                    onChange={handleOrdersPerPageChange}
+                                                    className="border border-gray-500 rounded p-1 pointer-cursor mx-2"
+                                                >
+                                                    <option value={5}>5</option>
+                                                    <option value={10}>10</option>
+                                                    <option value={15}>15</option>
+                                                    <option value={20}>20</option>
+                                                    <option value={50}>50</option>
+                                                </select>
+                                                <label htmlFor="doctorsPerPage">doctors per page</label>
+                                            </div>
+                                            <div>
+                                                {/* Search Input */}
+                                                <div className="flex justify-center rounded-l-lg group">
+                                                    <div className='flex justify-center items-center border border-gray-500 border-r-0 p-3 rounded-l-full  text-black font-extrabold text-shadow-xl'>
+                                                        <ImSearch />
+                                                    </div>
+                                                    <input
+                                                        type="text"
+                                                        placeholder="Search doctors"
+                                                        value={searchTerm}
+                                                        onChange={handleSearch}
+                                                        className="border border-gray-500 border-l-0 px-3 py-1 rounded-r-full focus:outline-none"
+                                                    />
+                                                </div>
+                                            </div>
+                                        </div>
+                                        {
+                                            filteredDoctors.length !== 0
+                                                ?
+                                                <>
+                                                    <div className="overflow-x-auto mb-3">
+                                                        <table className="table">
+                                                            {/* head */}
+                                                            <thead>
+                                                                <tr>
+                                                                    <th className="text-center">Sl. No.</th>
+                                                                    <th className="text-center">Doctor ID</th>
+                                                                    <th>Doctor Name</th>
+                                                                    <th>Visiting Address</th>
+                                                                    <th className='text-center'>Actions</th>
+                                                                </tr>
+                                                            </thead>
+                                                            <tbody>
+                                                                {
+                                                                    currentDoctors.map((doctor, idx) => (
+                                                                        <DoctorCard
+                                                                            idx={startIndex + idx + 1}
+                                                                            key={doctor._id}
+                                                                            doctor={doctor}
+                                                                            refetch={refetch}
+                                                                        />
+                                                                    ))
+                                                                }
+                                                            </tbody>
+                                                        </table>
+                                                    </div>
+
+                                                    {/* Pagination */}
+                                                    <div className="flex justify-center items-center mb-10">
+                                                        <div
+                                                            className={`mx-1 px-3 py-1 rounded-lg flex justify-center items-center ${currentPage === 1 ? 'cursor-not-allowed opacity-50' : 'cursor-pointer'}`}
+                                                            onClick={currentPage !== 1 ? () => changePage(currentPage - 1) : null}
+                                                            aria-disabled={currentPage === 1}
+                                                        >
+                                                            <span className='flex justify-between items-center text-black'>
+                                                                <BsArrowLeftCircleFill className='h-6 w-6' />
+                                                            </span>
+                                                        </div>
+                                                        <div className='flex justify-center items-center'>
+                                                            {
+                                                                Array.from({ length: totalPages }, (_, index) => (
+                                                                    <button
+                                                                        key={index}
+                                                                        className={`mx-1 flex justify-center items-center w-6 h-6 border border-black rounded-full ${currentPage === index + 1 ? 'bg-[#3B82F6] text-white font-mono font-extrabold border-2 border-green-900' : ''
+                                                                            }`}
+                                                                        onClick={() => changePage(index + 1)}
+                                                                    >
+                                                                        {index + 1}
+                                                                    </button>
+                                                                ))
+                                                            }
+                                                        </div>
+                                                        <div
+                                                            className={`mx-1 px-3 py-1 rounded-[4px] flex justify-center items-center ${currentPage === totalPages ? 'cursor-not-allowed opacity-50' : 'cursor-pointer'}`}
+                                                            onClick={currentPage !== totalPages ? () => changePage(currentPage + 1) : null}
+                                                            aria-disabled={currentPage === totalPages}
+                                                        >
+                                                            <span className='flex justify-between items-center text-black'>
+                                                                <BsArrowRightCircleFill className='h-6 w-6' />
+                                                            </span>
+                                                        </div>
+                                                    </div>
+                                                </>
+                                                :
+                                                <>
+                                                    <p className="text-gray-600 font-mono font-extrabold text-center mb-6">
+                                                        No result found.
+                                                    </p>
+                                                </>
+                                        }
+                                    </div>
+                                </>
+                        }
+                    </>
+                    :
+                    <>
+                        <p className="text-gray-600 font-mono font-extrabold">
+                            No customer(s) found.
+                        </p>
+                    </>
+            }
         </div>
     );
 };
